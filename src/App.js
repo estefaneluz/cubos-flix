5fd488d23983438f90e4b19e78c1e4ceeaa088c4
estefaneluz <estefanecortes@gmail.com>
2021-06-13 19:52:32 -0300

    html do Card

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import './App.css'
+import Card from './components/Card'
 
 function App() {
-  return <div className="app"></div>
+  return (
+    <div className="app">
+      <h2>Top Filmes</h2>
+      <Card />
+    </div>
+  )
 }
 
 export default App
---
+++ src/components/Card/index.js
@@ -0,0 +1,15 @@
+function Card() {
+  return (
+    <div className="card-movie">
+      <div className="card-info">
+        <h3>Nome do Filme</h3>
+        <div>6.5</div>
+      </div>
+      <button className="">
+        Sacola <span>R$ 9,5</span>
+      </button>
+    </div>
+  )
+}
+
+export default Card
