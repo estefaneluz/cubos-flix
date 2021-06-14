50532f716f67501a01a4e8506aede8d4240149ca
estefaneluz <estefanecortes@gmail.com>
2021-06-14 19:39:18 -0300

    populando o card com filmes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import './App.css'
 import Card from './components/Card'
+import './App.css'
+// import { useState } from 'react'
+import Movies from './data/data'
 
 function App() {
+  // const [filmes, setFilmes] = useState([])
+
   return (
     <div className="app">
-      <h2>Top Filmes</h2>
-      <Card />
+      <h2>Filmes</h2>
+      <Card filmes={Movies} />
     </div>
   )
 }
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,31 +1,40 @@
 import './style.css'
 import '../../App.css'
 
-function Card() {
+function Card({ filmes }) {
   return (
-    <div className="card-movie">
-      <div className="card-info">
-        <h3>Cruella</h3>
-        <div className="card-star">
-          <svg
-            width="13"
-            height="12"
-            viewBox="0 0 13 12"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M6.5 0L7.95934 4.49139H12.6819L8.86126 7.26722L10.3206 11.7586L6.5 8.98278L2.6794 11.7586L4.13874 7.26722L0.318133 4.49139H5.04066L6.5 0Z"
-              fill="#FBCD6E"
-            />
-          </svg>
-          6.5
+    <>
+      {filmes.map((filme) => (
+        <div
+          className="card-movie"
+          style={{
+            background: `linear-gradient(rgba(0, 0, 0, 0.3) 100%, rgba(0, 0, 0, 0.3) 100%), url(${filme.poster_path}) no-repeat center / cover`,
+          }}
+        >
+          <div className="card-info">
+            <h3>{filme.title}</h3>
+            <div className="card-star">
+              <svg
+                width="13"
+                height="12"
+                viewBox="0 0 13 12"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M6.5 0L7.95934 4.49139H12.6819L8.86126 7.26722L10.3206 11.7586L6.5 8.98278L2.6794 11.7586L4.13874 7.26722L0.318133 4.49139H5.04066L6.5 0Z"
+                  fill="#FBCD6E"
+                />
+              </svg>
+              {filme.vote_average}
+            </div>
+          </div>
+          <button>
+            Sacola <span>R$ {filme.price}</span>
+          </button>
         </div>
-      </div>
-      <button>
-        Sacola <span>R$ 9,5</span>
-      </button>
-    </div>
+      ))}
+    </>
   )
 }
 
