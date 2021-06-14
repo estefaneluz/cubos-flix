ca09aaa1e7b51ae009538f77c443b4b9998c45f4
estefaneluz <estefanecortes@gmail.com>
2021-06-14 19:54:26 -0300

    estilizando container e body

--- src/App.js
+++ src/App.js
@@ -9,7 +9,9 @@ function App() {
   return (
     <div className="app">
       <h2>Filmes</h2>
-      <Card filmes={Movies} />
+      <div className="container">
+        <Card filmes={Movies} />
+      </div>
     </div>
   )
 }
